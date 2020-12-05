ba6cfbc6139755fa004767e742031bdaa2357c9c
Eunchan Park <freetime4y@gmail.com>
2020-12-06 00:11:49 +0200

    add git, ros, x11

---
+++ setup.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#install 
+sudo apt update
+sudo apt install -y openssh-server x11vnc
+
+
+#ros
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+
+echo "[ press enter to contine ]"
+read
+
+sudo apt update
+
+sudo apt install -y ros-noetic-desktop-full
+
+
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
