a25cc98ac7ae6fe603c8a2ca9f8c6d56f8b71731
Eunchan Park <freetime4y@gmail.com>
2020-12-06 16:53:03 +0200

    add catkin tool

--- setup.bash
+++ setup.bash
@@ -38,3 +38,10 @@ sudo apt install -y ros-noetic-desktop-full
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
+
+
+echo "[catkin install]"
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list'
+wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
+sudo apt-get update
+sudo apt install python3-catkin-tools
